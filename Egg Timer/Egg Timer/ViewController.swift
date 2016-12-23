5264690f94b4173288580adf2e0cb0ca48aaaa95
Eduardo <eduardomelo29@gmail.com>
2016-12-23 13:26:36 -0300

    Create the timer inside the main controller

--- Egg Timer/Egg Timer/ViewController.swift
+++ Egg Timer/Egg Timer/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var timer = Timer();
+    var time = 210;
+    
+    func decreaseTimer(){
+        if (time > 0){
+            time = time - 1;
+            timeLabel.text = String(time);
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func playTapped(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.decreaseTimer), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pauseTapped(_ sender: Any) {
+        timer.invalidate();
+    }
+    
+    @IBAction func minusTenTapped(_ sender: Any) {
+        if (time >= 10) {
+            time = time - 10;
+        } else {
+            time = 0;
+        }
+        
+        timeLabel.text = String(time);
+    }
+    
+    @IBAction func plusTenTapped(_ sender: Any) {
+        time = time + 10;
+        timeLabel.text = String(time);
+    }
+    
+    @IBAction func resetTapped(_ sender: Any) {
+        time = 210;
+        timeLabel.text = String(time);
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
